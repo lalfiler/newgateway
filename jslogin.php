3cd9707e26a2bc44e5615d5cd6f9ffa8bcfc8861
Noah <nnaamad@ngsc.org>
2019-01-04 14:38:14 -0800

    fix typos

--- jslogin.php
+++ jslogin.php
@@ -13,12 +13,12 @@ $email = $_POST["email"];
 $password = md5($_POST["password"]);
 
 
-$sql = "SELECt * from jobseekers WHERE email = '".$email."' AND password = '".$password."' limit 1";
+$sql = "SELECT * from jobseekers WHERE email = '".$email."' AND password = '".$password."' limit 1";
 
 $result = mysqli_query($link, $sql);
 
 if(mysqli_num_rows($result)==1){
-echo "You have successfuly logged in";
+echo "You have successfully logged in";
 exit();
 
 
