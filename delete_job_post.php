d0d6324c3150386ae3b8f92b56f96ef344bfbbdb
Noah <nnaamad@ngsc.org>
2019-01-25 15:17:14 -0800

    enable deletion of job posting

---
+++ delete_job_post.php
@@ -0,0 +1,42 @@
+<?php
+
+ 
+try {
+	//include_once 'accesscontrol.php';
+	session_start();
+	$email = $_SESSION['email'];
+	echo "<script> console.log('Hello, " . $email . "! ')</script>";
+	
+	$link = mysqli_connect("localhost", "root", "", "job_board_db");
+
+	// Check connection
+	if($link === false){
+		die("ERROR: Could not connect. " . mysqli_connect_error());
+	}
+
+	// Grab company from database
+	$companyID_object = mysqli_query($link, "SELECT id from employers WHERE email = '".$email."'");
+	$companyID = (mysqli_fetch_row($companyID_object))[0];
+	echo "<script> console.log('companyID is: " . $companyID . "!')</script>";
+		
+    // get record ID
+    $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+ 
+    // delete query
+    $query = "DELETE FROM postajob WHERE id = '" .$id. "'";
+    $stmt = $link->prepare($query);
+     
+    if($stmt->execute()){
+        // redirect to read records page and 
+        // tell the user record was deleted
+        header('Location: myjobs.php?action=deleted');
+    }else{
+        die('Unable to delete record.');
+    }
+}
+ 
+// show error
+catch(PDOException $exception){
+    die('ERROR: ' . $exception->getMessage());
+}
+?>
--- myjobs.php
+++ myjobs.php
@@ -23,6 +23,13 @@
 		die("ERROR: Could not connect. " . mysqli_connect_error());
 	}
 
+	$action = isset($_GET['action']) ? $_GET['action'] : "";
+	
+	// if it was redirected from delete.php
+	if($action=='deleted'){
+		echo "<div class='alert alert-success'>Job post was successfully deleted.</div>";
+	}
+	
 	// Grab company from database
 	$companyID_object = mysqli_query($link, "SELECT id from employers WHERE email = '".$email."'");
 	$companyID = (mysqli_fetch_row($companyID_object))[0];
@@ -43,13 +50,23 @@
 		$jobID = $row['id'];
 		echo "<b>
 		$jobTitle <a href='job.php?id={$jobID}'>View This Job</a><br>
-		$experienceLevel <a href='update_job_post.php?id={$jobID}'>Edit This Job</a><br>
-		<a href='job.php?id={$jobID}'>Delete This Job</a></b>
+		$experienceLevel <a href='update_job_post.php?id={$jobID}'>Edit This Job</a><br>";
+		echo "<a href='#' onclick='delete_job({$jobID});' class='btn btn-danger'>Delete This Job</a></b>
 		<hr>
 		<br>
 		<br>";
 	}
 ?>
+
+<script type="text/javascript">
+	function delete_job(id){
+		console.log("delete_job is running!");
+		var confirmation = confirm("Are you sure you want to delete this job posting?");
+		if(confirmation){
+			window.location = 'delete_job_post.php?id=' + id;
+		}
+	}
+</script>
 
     <footer>
         <p>&copy; 2018 New GateWay Solutions Corporation</p>
