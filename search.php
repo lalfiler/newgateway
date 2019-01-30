220136fe8ae1b5ce5312e13667f22ca717c6c0a4
Noah <nnaamad@ngsc.org>
2019-01-30 15:24:22 -0800

    add search for jobs functionality

--- search.php
+++ search.php
@@ -1,29 +1,24 @@
 <?php
    $link = mysqli_connect("localhost", "root", "", "job_board_db");
- 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-$set = $_POST['jobTitle'];
-$get = $_POST['address'];
-$sql = ("SELECT * FROM postajob WHERE jobTitle, category LIKE '%$set%' AND city, zip LIKE '%$get%'");
-$result = mysqli_query($link, $sql);
-$num_rows = mysqli_num_rows($result);
+$jobTitle = $_POST['jobTitle'];
+$city = $_POST['address'];
+$sql = ("SELECT * FROM postajob WHERE jobTitle LIKE '%$jobTitle%' AND city OR zip LIKE '%$city%'");
+$result = $link->query($sql);
 
-echo "<script>console.log('result is: " . $result . "');</script>";
-echo "<script>console.log('num_rows is: " . $num_rows . "');</script>";
-
-
-if ($num_rows > 0) {
-    echo "<table><tr><th>Name</th></tr>";
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>Results</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["jobTitle"]."</td><td>".$row["category"]."</td><td>".$row["city"]."</td><td>".$row["zip"]."</td></tr>";
+        echo "<tr><td>".$row["jobTitle"]."</td><td>".$row["city"]."</td><td>".$row["zip"]."</td></tr>";
     }
     echo "</table>";
 } else {
     echo "0 results";
 }
- $link->close();
- ?> 
+$link->close();
+?>
